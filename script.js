b9094d858c32e0082ef5e3c3f357f40f8f9c031b
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-05-17 02:13:01 +0530

    Completed 2nd page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function navAnimation(){
     var nav = document.querySelector(".nav-part2")
 nav.addEventListener('mouseenter',function(){
@@ -42,4 +43,36 @@ nav.addEventListener('mouseleave',function(){
 })
 
 }
-// navAnimation()
+navAnimation()
+
+function page2Animation(){
+    var rightElems = document.querySelectorAll("#right-elem")
+
+rightElems.forEach(function(elem) {
+    elem.addEventListener("mouseenter", function () {
+
+
+
+
+        gsap.to(elem.childNodes[3], {
+          opacity:1,
+            scale: 1
+        })
+    })
+    elem.addEventListener("mousemove", function (dets) {
+
+        gsap.to(elem.childNodes[3], {
+            x: dets.x - elem.getBoundingClientRect().x - 90,
+            y: dets.y - elem.getBoundingClientRect().y - 215
+        })
+    })
+
+    elem.addEventListener("mouseleave", () => {
+        gsap.to(elem.childNodes[3], {
+            opacity:0,
+              scale: 0
+          })
+    });
+});
+}
+page2Animation()
