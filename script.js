a11e98c2d9b91d1ece0bca12e9d9dda5f31950ff
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-05-19 19:01:07 +0530

    completed page3 video playing and pausing function

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ nav.addEventListener('mouseleave',function(){
 })
 
 }
-navAnimation()
+
 
 function page2Animation(){
     var rightElems = document.querySelectorAll("#right-elem")
@@ -72,4 +72,29 @@ rightElems.forEach(function(elem) {
     });
 });
 }
-page2Animation()
+
+function page3Animation() {
+    var page3Center = document.querySelector(".page3 .vedioIcon")
+    var video = document.querySelector(".page3 video")
+
+    page3Center.addEventListener("click", function () {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0
+        })
+    })
+    video.addEventListener("click", function () {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    })
+
+}
+page3Animation(); 
+// navAnimation()
+page2Animation();
